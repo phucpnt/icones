336d39359263bb52bf5307cbe8067dfa2dd003a9
Phuc PNT <pn.truongphuc@gmail.com>
2023-04-29 19:16:57 +0000

    chore

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,9 @@ export const isElectron = import.meta.env.MODE === 'electron'
 export const isVSCode = location.protocol === 'vscode-webview:'
 export const isLocalMode = isElectron || isVSCode
 
-export const basePath = isVSCode ? window.baseURI : '/'
-export const staticPath = isVSCode
-  ? window.staticURI
-  : (isElectron && import.meta.env.PROD) ? '../../app.asar/dist' : ''
+export const basePath = process.env.NODE_ENV === 'production' ? '/icones' : isVSCode ? window.baseURI : '/'
+export const staticPath = process.env.NODE_ENV === 'production'
+  ? '/icones'
+  : isVSCode
+    ? window.staticURI
+    : (isElectron && import.meta.env.PROD) ? '../../app.asar/dist' : ''
